bdf0f02d308fef5fda66e578b279fb57359bd73d
graziani <alicia.graziani.ag@gmail.com>
2022-10-05 18:51:37 +0200

    Updates

---
+++ precache-manifest.b14c755fddabd5dcec89cc5bcf5f35eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0de9643996783e4b23a",
+    "url": "/Kasa/static/css/main.08f0cb93.chunk.css"
+  },
+  {
+    "revision": "f0de9643996783e4b23a",
+    "url": "/Kasa/static/js/main.f0de9643.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/Kasa/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "a44e1e7d8a85734529d0",
+    "url": "/Kasa/static/js/2.a44e1e7d.chunk.js"
+  },
+  {
+    "revision": "04f681fddb41486b724c",
+    "url": "/Kasa/static/js/runtime~main.04f681fd.js"
+  },
+  {
+    "revision": "97f2cc2442ac5b27d6670a938c74235a",
+    "url": "/Kasa/static/media/Vector.97f2cc24.svg"
+  },
+  {
+    "revision": "617707a36a8f4b4328f2f4976cea5a7a",
+    "url": "/Kasa/static/media/LOGO.617707a3.jpg"
+  },
+  {
+    "revision": "318727ec6ceb182a5ea06a90ecc0aaee",
+    "url": "/Kasa/static/media/logoFooter.318727ec.svg"
+  },
+  {
+    "revision": "097d3de49124f2a8b7325bbe7e9ab7e1",
+    "url": "/Kasa/static/media/imageAbout.097d3de4.jpg"
+  },
+  {
+    "revision": "0ac31d1595b4d47ef69ddcb6d4d45c37",
+    "url": "/Kasa/static/media/imageAboutMobile.0ac31d15.jpg"
+  },
+  {
+    "revision": "b78c1aba320f63c43f21ab7fa79309d0",
+    "url": "/Kasa/static/media/ImgHome.b78c1aba.jpg"
+  },
+  {
+    "revision": "6ccafa3d41f9015a85ed8437d44090ae",
+    "url": "/Kasa/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Kasa/precache-manifest.b14c755fddabd5dcec89cc5bcf5f35eb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Kasa/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
