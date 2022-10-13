f0a3301a325f71fcd5bf83cfc73a5893b98f60bf
graziani <alicia.graziani.ag@gmail.com>
2022-10-13 13:00:07 +0200

    commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../navbar/navbar'
 import LOGO from '../../assets/LOGO.jpg'
-import '../header/header.css'
+import './header.css'
 
 export default function Header() {
   return (
